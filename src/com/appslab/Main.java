3bfc8d55ef726ff129425704cf3ee4ed3e0d21dc
MartinMartiniak <martiniak.martin15@gmail.com>
2021-09-14 09:43:38 +0200

    make a formula for showing us distance

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,12 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+
+
+        double length=0.2;
+        double height=0.4;
+        double towerHeight=100;
+        double distance=(towerHeight/height)+(towerHeight/height)*length;
+        System.out.println(distance);
     }
 }
