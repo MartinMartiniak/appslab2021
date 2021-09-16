d6591984b904e5a130406859adcd14eb6775e1ba
MartinMartiniak <martiniak.martin15@gmail.com>
2021-09-16 08:30:46 +0200

    check for equality of 3 integers returning the number of how many are equal

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,32 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int a=2;
+        int b=1;
+        int c=0;
+        int d=0;
+        if (a==b){
+            d=d+1;
+        }
+        else if (a!=b){
+            d=d+0;
+        }
+        if (a==c){
+            d=d+1;
+        }
+        else if (a!=c){
+            d=d+0;
+        }
+
+        if (b==c){
+            d=d+1;
+        }
+        else if (b!=c){
+            d=d+0;
+        }
+        if (d==1){
+            d=2;
+        }
+        System.out.println(d);
     }
 }
