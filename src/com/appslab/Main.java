fe2bb69f285bd0c83cf31dddb2879b8ec18025f7
MartinMartiniak <martiniak.martin15@gmail.com>
2021-09-16 08:53:23 +0200

    check for the possibility of making a triangle via the three integers given

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,32 +3,29 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        int a=2;
-        int b=1;
-        int c=0;
-        int d=0;
-        if (a==b){
-            d=d+1;
-        }
-        else if (a!=b){
-            d=d+0;
-        }
-        if (a==c){
-            d=d+1;
-        }
-        else if (a!=c){
-            d=d+0;
-        }
+     int a=2;
 
-        if (b==c){
-            d=d+1;
-        }
-        else if (b!=c){
-            d=d+0;
-        }
-        if (d==1){
-            d=2;
-        }
-        System.out.println(d);
+     int b=3;
+
+     int c=3;
+
+     int truth=0;
+
+     if (a+b<=c){
+         truth=truth+1;
+     }
+     if (a+c<=b){
+         truth=truth+1;
+     }
+     if (c+b<=a){
+         truth=truth+1;
+     }
+     if (truth>=1){
+         System.out.println("false");
+     }
+     else if (truth==0){
+         System.out.println("true");
+     }
+
     }
 }
