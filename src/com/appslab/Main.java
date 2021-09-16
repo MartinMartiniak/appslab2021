df4f959d298530bef9c5d371cfe1a94901246361
MartinMartiniak <martiniak.martin15@gmail.com>
2021-09-16 09:12:25 +0200

    look if people can get the given amount of slices of pie from already given number of slices

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,29 +3,17 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-     int a=2;
+     int a=24;
 
-     int b=3;
+     int b=12;
 
-     int c=3;
+     int c=2;
 
-     int truth=0;
-
-     if (a+b<=c){
-         truth=truth+1;
-     }
-     if (a+c<=b){
-         truth=truth+1;
-     }
-     if (c+b<=a){
-         truth=truth+1;
-     }
-     if (truth>=1){
-         System.out.println("false");
-     }
-     else if (truth==0){
-         System.out.println("true");
-     }
-
+if (b*c<=a){
+    System.out.println("True");
+}
+else if (b*c>a){
+    System.out.println("false");
+}
     }
 }
