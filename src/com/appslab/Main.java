175662923a00892504354443db3655938b67fb36
Martin Martiniak <martiniak.martin15@gmail.com>
2021-09-29 19:44:31 +0200

    Reverse the given word and check if it is the same as the given.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,32 +3,15 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        int a=2;
-        int b=1;
-        int c=0;
-        int d=0;
-        if (a==b){
-            d=d+1;
-        }
-        else if (a!=b){
-            d=d+0;
-        }
-        if (a==c){
-            d=d+1;
-        }
-        else if (a!=c){
-            d=d+0;
-        }
 
-        if (b==c){
-            d=d+1;
+        String word = "radar";
+        StringBuffer buffer = new StringBuffer(word);
+        buffer.reverse();
+        String data = buffer.toString();
+        if(word.equals(data)){
+            System.out.println("Given String is palindrome");
+        }else{
+            System.out.println("Given String is not palindrome");
         }
-        else if (b!=c){
-            d=d+0;
-        }
-        if (d==1){
-            d=2;
-        }
-        System.out.println(d);
     }
 }
