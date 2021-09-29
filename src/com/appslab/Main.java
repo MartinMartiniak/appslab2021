a63fbe574f93c6332cfe9662cc6f6f3ce75402a1
Martin Martiniak <martiniak.martin15@gmail.com>
2021-09-29 20:09:23 +0200

    Select numbers in your array figure out their length, multiply every number by that

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,21 +1,29 @@
 package com.appslab;
 
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array_nums = {643, 1251, 28, 16, 3};
-        System.out.println("Original Array: "+ Arrays.toString(array_nums));
-        int max_val = array_nums[0];
-        int min = array_nums[0];
-        for(int i = 1; i < array_nums.length; i++)
-        {
-            if(array_nums[i] > max_val)
-                max_val = array_nums[i];
-            else if(array_nums[i] < min)
-                min = array_nums[i];
+        Scanner cc = new Scanner(System.in);
+
+        System.out.println("Select first number:");
+        int a = cc.nextInt();
+
+        System.out.println("Select second number:");
+        int b = cc.nextInt();
+
+        System.out.println("Select third number:");
+        int c = cc.nextInt();
+
+        System.out.println("Select fourth number:");
+        int d = cc.nextInt();
+
+        int array1[] = {a, b, c, d};
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i] * array1.length);
         }
-        System.out.println("Difference between the largest and smallest values of the said array: "+(max_val-min));
+
     }
 }
