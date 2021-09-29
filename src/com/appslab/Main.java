110b5e5b4a93b1269cd6a3716c8222bd8e3e19e1
Martin Martiniak <martiniak.martin15@gmail.com>
2021-09-29 20:02:11 +0200

    take max value and minimal value, return the difference

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,17 +1,21 @@
 package com.appslab;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        String word = "radar";
-        StringBuffer buffer = new StringBuffer(word);
-        buffer.reverse();
-        String data = buffer.toString();
-        if(word.equals(data)){
-            System.out.println("Given String is palindrome");
-        }else{
-            System.out.println("Given String is not palindrome");
+        int[] array_nums = {643, 1251, 28, 16, 3};
+        System.out.println("Original Array: "+ Arrays.toString(array_nums));
+        int max_val = array_nums[0];
+        int min = array_nums[0];
+        for(int i = 1; i < array_nums.length; i++)
+        {
+            if(array_nums[i] > max_val)
+                max_val = array_nums[i];
+            else if(array_nums[i] < min)
+                min = array_nums[i];
         }
+        System.out.println("Difference between the largest and smallest values of the said array: "+(max_val-min));
     }
 }
